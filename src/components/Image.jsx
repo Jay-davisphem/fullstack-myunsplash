04f34f46d912e2e97c34435b1adaf14a6a0824ba
David Oluwafemi <davidoluwafemi178@gmail.com>
2023-04-30 21:10:43 +0100

    add more

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,15 +4,20 @@ export default function ({ img }) {
   const [hovered, setHovered] = useState(false);
   return (
     <div
-      className="rounded-lg"
+      className="relative rounded-lg cursor-pointer"
       onMouseEnter={(e) => setHovered(true)}
       onMouseLeave={(e) => setHovered(false)}
     >
       {hovered && (
-        <Button
-          title="delete"
-          moreStyles="bg-transparent rounded-3xl border-2 border-[#EB5757] px-5 py-1 font-normal text-[#EB5757]"
-        />
+        <>
+          <Button
+            title="delete"
+            moreStyles="bg-transparent absolute right-4 top-4 rounded-3xl border-2 border-[#EB5757] px-5 py-1 font-normal text-[#EB5757]"
+          />
+          <p className="absolute font-black text-white bottom-4 left-4">
+            {img.label}
+          </p>
+        </>
       )}
       <img src={img.url} alt={img.label} className="rounded-lg  w-[20vw]" />
     </div>
